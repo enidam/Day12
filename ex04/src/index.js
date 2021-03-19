50f2e1945814fc935805117ea9263f54ec65f3f0
enida <enida_mujezin_16@yahoo.com>
2021-03-20 00:49:52 +0100

    nista

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function sumFibonacci(num) {
+    if (num === 1){
+        return [0,1];
+    } else {
+        
+    }
+
+
+};
+console.log(sumFibonacci(1));
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
+
+module.exports = sumFibonacci;
